808fa04b3f1ba17de640f971367f21e60a5cbff1
Danx016 <danilorodelo355@gmail.com>
2026-09-09 14:16:29 -0500

    chore(release): bump version to 1.0.90 [v1.0.90]

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -76,7 +76,7 @@ class ReleaseInfo {
 }
 
 class UpdateService {
-  static String currentVersion = '1.0.89';
+  static String currentVersion = '1.0.90';
   static const MethodChannel _channel = MethodChannel('com.groovy.music/app_updater');
 
   static const String _apiUrl =
